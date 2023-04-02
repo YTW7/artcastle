f65a73f1493c22fb934145d007a451a3aa46c5b6
YTW7 <myahya96@gmail.com>
2023-04-02 22:31:40 +0530

    v 1.0

---
+++ components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from "../styles/CreateProduct.module.css";
+const Home = () => {
+  return (
+    <div class="bg-white " className={styles.home_bg}>
+        <br />
+     {/* <p class="header text-black text-3xl bg-white ml-12 mr-12 bg-opacity-90 rounded-lg pt-2 pb-2" >  Get paid in $BONK for Creating Memes!</p> */}
+
+     <div class="bg-bonk rounded-xl mx-12 p-6 mt-9 mb-28 bg-opacity-90">
+     <p class="header text-white text-5xl font-black " >   A Stage for Artists</p>
+     <br />
+     <p class="header text-white text-3xl" >  </p>
+     <br />
+     <p class="header text-white text-5xl font-black" >Treasure for Art-Seekers</p>
+     <br />
+     </div>
+          {/* <p className="sub-text text-black">Have a doubt? Crack it out with CRACK-A-DOUBT</p> */}
+          <br />
+</div>
+  )
+}
+
+export default Home
---
+++ components/Navbar.js
@@ -0,0 +1,72 @@
+import React,{useState} from 'react'
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { PublicKey } from '@solana/web3.js';
+import { useWallet } from '@solana/wallet-adapter-react';
+import dynamic from "next/dynamic";
+import CreateProduct from "../components/CreateProduct";
+import styles from "../styles/Product.module.css";
+
+const Navbar = () => {
+  // Dynamic import `WalletMultiButton` to prevent hydration error
+  const { publicKey } = useWallet();
+  const [creating, setCreating] = useState(false);
+ // const isOwner = ( publicKey ? publicKey.toString() === process.env.NEXT_PUBLIC_OWNER_PUBLIC_KEY : false );
+
+
+const WalletMultiButtonDynamic = dynamic(
+  async () =>
+    (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+  { ssr: false }
+);
+
+// This will fetch the users' public key (wallet address) from any wallet we support
+
+
+const renderNotConnectedContainer = () => (
+
+    <div className="button-container" >
+      <WalletMultiButtonDynamic className="cta-button connect-wallet-button" />
+    </div>    
+);
+
+  return (
+    <>
+    <header class="text-gray-600 body-font bg-gradient-to-b from-purple-500 to-green-500  " >
+  <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <a class="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+      {/* <img href="/" src="/logo.png" class="w-10 h-10 rounded-lg" alt="logo" /> */}
+      <a href="/" class="text-navblue text-2xl ml-2 font-black">ART CASTLE</a>
+      
+    </a>
+    <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 flex flex-wrap items-center text-base justify-center">
+      {/* <a href="/" class="mr-5 text-navblue hover:text-white font-black"></a> */}
+      
+    </nav>
+    
+    <div  class="  flex flex-col items-center text-base justify-center font-black  ">
+    {publicKey ? 
+            <button  className="create-product-button" onClick={() => setCreating(!creating)}>
+              {creating ? "Close ✖" : "+ Share Your Art"}
+            </button>
+            : ""}
+ 
+    </div>
+    
+    <div class="bg-gradient-to-b from-purple-400 to-green-400  rounded">
+    
+  <WalletMultiButton>
+{/* We only render the connect button if public key doesn't exist */}
+
+{publicKey ? publicKey.toString().slice(0,4)+"..."+publicKey.toString().slice(-3,) : renderNotConnectedContainer()}
+
+    </WalletMultiButton>
+    </div>
+  </div>
+</header>
+{creating && <CreateProduct />}
+    </>
+    
+  )
+}
+
+export default Navbar
